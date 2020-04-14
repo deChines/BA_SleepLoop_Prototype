c869e9e089a02eb701c68a50abbffedd7d5f72c0
Randy Main PC <35844169+deChines@users.noreply.github.com>
2020-04-15 01:52:15 +0200

    start
    Initiate current state to git

---
+++ Assets/GoogleARCore/Examples/AugmentedFaces/Scripts/ARSelector.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARSelector : MonoBehaviour
+{
+  [SerializeField] GameObject[] targets;
+
+   GameObject currentTarget;
+   int targetIndex;
+
+
+   void Start()
+   {
+       ChangeTarget();
+   }
+
+
+   void ChangeTarget()
+   {
+       if (targets.Length > targetIndex)
+       {
+           if (currentTarget != null)
+           {
+               currentTarget.SetActive(false);
+           }
+
+           currentTarget = targets[targetIndex];
+           currentTarget.SetActive(true);
+           targetIndex++;
+       }
+   }
+
+
+   public void OnClickButton()
+   {
+       ChangeTarget();
+   }
+}
